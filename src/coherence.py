af41b7acf35f8036b019a61836f916c5e7614e76
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-11-11 22:46:35 +0530

    added coherence sweep module

---
+++ src/coherence.py
@@ -0,0 +1,40 @@
+"""coherence sweep helper.
+
+Trains LDA at several values of k and returns coherence scores so we can
+pick a sensible number of topics.
+"""
+import logging
+
+from gensim.models import CoherenceModel
+
+from .lda import train_lda
+
+
+log = logging.getLogger(__name__)
+
+
+def coherence_sweep(token_lists, k_min=5, k_max=25, k_step=5,
+                    metric="c_v", passes=5, iterations=50, random_state=42):
+    """run train+score over a range of k. returns list of (k, score)."""
+    out = []
+    for k in range(k_min, k_max + 1, k_step):
+        log.info("training k=%d", k)
+        lda, dictionary, _ = train_lda(
+            token_lists,
+            num_topics=k,
+            passes=passes,
+            iterations=iterations,
+            random_state=random_state,
+        )
+        cm = CoherenceModel(
+            model=lda,
+            texts=token_lists,
+            dictionary=dictionary,
+            coherence=metric,
+        )
+        score = cm.get_coherence()
+        log.info("k=%d coherence=%.4f", k, score)
+        out.append((k, score))
+    return out
+</content>
+</invoke>
